6ad81632b88cbd1f34dee94762767e2e2f84ac69
hangytong <hangytong@gmail.com>
2025-11-19 15:35:21 +0800

    update

--- rewards.py
+++ rewards.py
@@ -32,7 +32,7 @@ try:
         all_rewards = get_all_rewards()
 
     # === STEP 1: Load CSV ===
-    file_path = MARKETS_DIR   # Change to your actual CSV file path
+    file_path = "markets.csv"   # Change to your actual CSV file path
 
     df = pd.read_csv(file_path, usecols=['question','market_id', 'side'], dtype={'question' : str,'market_id': str, 'side': str})
 
