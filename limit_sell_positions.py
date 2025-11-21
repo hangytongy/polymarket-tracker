f1f43445286b86a2561cf71b3ac441d377e3d802
hangytong <hangytong@gmail.com>
2025-11-21 09:56:39 +0800

    update

--- limit_sell_positions.py
+++ limit_sell_positions.py
@@ -31,7 +31,7 @@ if response.status_code == 200:
             event_slug = position['eventSlug']
             asset = position['asset']
             cond_id = position['conditionId']
-            market_id = position['eventId']
+            #market_id = position['eventId']
             total_size = float(position['size'])
             question = position['title']
             avg_price = position['avgPrice']
@@ -42,12 +42,12 @@ if response.status_code == 200:
                 continue
 
             #get tick size
-            url = f"https://gamma-api.polymarket.com/markets/{market_id}"
+            #url = f"https://gamma-api.polymarket.com/markets/{market_id}"
 
-            response = requests.get(url)
-            data = response.json()
-            tick = data['orderPriceMinTickSize']
-            decimal_places = len(str(tick).split(".")[1]) if "." in str(tick) else 0
+            #response = requests.get(url)
+            #data = response.json()
+            #tick = data['orderPriceMinTickSize']
+            #decimal_places = len(str(tick).split(".")[1]) if "." in str(tick) else 0
 
             #get order book price
             ob = get_orderbook_data(asset)
@@ -60,7 +60,8 @@ if response.status_code == 200:
 
             if price < avg_price and SET_NEVER_BELOW_COST:
                 print("skip, price is lower than avg price")
-                price = round(avg_price,decimal_places)
+                #price = round(avg_price,decimal_places)
+                price = avg_price
             
 
             sell_count = 0
