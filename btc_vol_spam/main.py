b60fd9deb1fd306b1d373ffa2e356e7220b35099
hangytong <hangytong@gmail.com>
2025-11-28 13:04:18 +0800

    update

--- btc_vol_spam/main.py
+++ btc_vol_spam/main.py
@@ -29,14 +29,14 @@ while True:
                 print(current_orders)
                 buy_orders = [order for order in current_orders if order['side'] == 'BUY']
                 if not buy_orders:
-                    if mid_price >= MAX_PRICE:
+                    if mid_price >= MAX_PRICE and mid_price < 0.99:
                         place_order(token_id, mid_price,size)
                         message = f"placed order for BTC at {mid_price} for timestamp {timestamp}"
                         send_telegram_message(message)
                     else:
                         print(f"mid price {mid_price} not at {MAX_PRICE}")
             else:
-                if mid_price >= MAX_PRICE:
+                if mid_price >= MAX_PRICE and mid_price < 0.99:
                     place_order(token_id, mid_price,size)
                     message = f"placed order for BTC at {mid_price} for timestamp {timestamp}"
                     send_telegram_message(message)
